93702fde805732ae0f538f62c4d1f7b0944712ae
John Borwick <borwick@vt.edu>
2014-06-19 13:03:37 -0400

    restructured to use subdirectories for each vm

--- packer-scripts/freebsd-10/setup.sh
+++ packer-scripts/freebsd-10/setup.sh
@@ -2,9 +2,9 @@
 # Credit to http://blog.codeship.io/2013/11/07/building-vagrant-machines-with-packer.html
 
 # Installing vagrant keys
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-cd ~/.ssh
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+cd ~vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 chown -R vagrant ~/.ssh
