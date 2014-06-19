a5fd46f9d8d4bdf202105546dc126d99b95464b4
John Borwick <borwick@vt.edu>
2014-06-19 17:11:26 -0400

    wipe /etc/sudoers + use sudo in vagrant setup

--- vagrant-scripts/centos-6.5/puppetclient.sh
+++ vagrant-scripts/centos-6.5/puppetclient.sh
@@ -2,6 +2,9 @@
 
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -Uvh epel-release-*.rpm
+
+sudo bash <<EOF
 yum -y install puppet
 service puppet start
 chkconfig puppet on
+EOF
