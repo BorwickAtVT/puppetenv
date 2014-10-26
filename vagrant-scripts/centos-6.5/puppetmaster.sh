57c8e4c69d921fd77cdc83fa234da654d4b09e93
John Borwick <john_borwick@pobox.com>
2014-10-26 19:41:38 -0400

    Use CentOS directory locations
    * logdir
    * rundir
    * ssldir

--- vagrant-scripts/centos-6.5/puppetmaster.sh
+++ vagrant-scripts/centos-6.5/puppetmaster.sh
@@ -17,6 +17,20 @@ ln -s /vagrant/puppet-config /etc/puppet
 cat > /etc/puppet/puppet.conf <<EOF
 certname = alpha
 dns_alt_names = alpha.dev, puppet, puppet.dev
+
+[main]
+    # The Puppet log directory.
+    # The default value is '$vardir/log'.
+    logdir = /var/log/puppet
+
+    # Where Puppet PID files are kept.
+    # The default value is '$vardir/run'.
+    rundir = /var/run/puppet
+
+    # Where SSL certificates are kept.
+    # The default value is '$confdir/ssl'.
+    ssldir = $vardir/ssl
+
 EOF
 
 sudo echo '*' > /etc/puppet/autosign.conf
