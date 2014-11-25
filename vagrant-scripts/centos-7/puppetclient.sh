f0db158166071632630cffee22ec5e6c10e70851
John Borwick <borwick@vt.edu>
2014-11-25 09:45:03 -0500

    systemctl for client

--- vagrant-scripts/centos-7/puppetclient.sh
+++ vagrant-scripts/centos-7/puppetclient.sh
@@ -4,6 +4,5 @@ rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 
 sudo bash <<EOF
 yum -y install puppet
-service puppet start
-chkconfig puppet on
+systemctl enable puppet.service
 EOF
