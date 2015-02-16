18c05c5ec800279b923f29f36f781021d00016b9
John Borwick <john_borwick@pobox.com>
2015-02-16 11:32:14 -0500

    yum makecache

--- vagrant-scripts/centos-7/theforeman.sh
+++ vagrant-scripts/centos-7/theforeman.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+# TODO -- workaround
+echo '#includedir /etc/sudoers.d' >> /etc/sudoers
+
 yum -y install epel-release
 yum -y install http://yum.theforeman.org/releases/1.7/el7/x86_64/foreman-release.rpm
 yum -y install foreman-installer
-foreman-installer
 
-# TODO -- workaround
-echo '#includedir /etc/sudoers.d' >> /etc/sudoers
+foreman-installer --foreman-admin-password=vagrant \
+		  --enable-foreman-compute-ec2 \
+		  --enable-foreman-compute-gce
+
+# link puppet dir and pull in Foreman stuff
+mv /etc/puppet/ /etc/puppet-orig/
+mkdir /etc/puppet
+git clone /vagrant/puppet-config/ /etc/puppet/
+rsync -az /etc/puppet-orig/ /etc/puppet/
+
+echo '*' > /etc/puppet/autosign.conf
+
 
 bash <<EOF
 firewall-cmd --permanent --zone=public --add-port=69/udp
